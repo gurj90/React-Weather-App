20c9330f76e29bf562ade0c6fb3b8fc80dc5fcba
gurj90 <gurj08@gmail.com>
2018-04-19 16:28:08 +0100

    test

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import Weather from "./components/Weather";
 
 const API_KEY = "866cdc553eb44193af29d08b7eb7f908";
 
-class App extends React.Component{
-    render (){
-      return (
-        <div>
-          <Titles/>
-          <Form/>
-          <Weather/>
-        </div>
-      );
-    }
+class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`)
+,ll
+  }
+  render() {
+    return (
+      <div>
+        <Titles />
+        <Form />
+        <Weather />
+      </div>
+    );
+  }
 }
 
 export default App;
