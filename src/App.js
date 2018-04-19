dedbf46d293cca791e15fabc89fd88fb34290b1d
gurj90 <gurj08@gmail.com>
2018-04-19 16:43:47 +0100

    test2

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ const API_KEY = "866cdc553eb44193af29d08b7eb7f908";
 
 class App extends React.Component {
   getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`)
-,ll
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`)
+const data = await api_call.json()
   }
   render() {
     return (
